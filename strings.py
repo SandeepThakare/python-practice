880ffaa7186c4efd503b659775a1ed7aed285668
SandeepThakare <sandythakare9@gmail.com>
2019-01-11 18:45:33 +0530

    last commit today

--- strings.py
+++ strings.py
@@ -35,5 +35,27 @@ print("#Split by a specific element (doesn't include the element that was split 
 
 print("Split word by latter 'W' - a.split('w')", a.split('w'))
 
+print('\n-----String Formatting-----')
 
+print("I'm going to inject %s here." %'something')
+
+print('\n .format() method')
+
+print('Add something inside curly braces, {}'.format('your name please'))
+
+print('Add something by indexing {1} {2} {0}'.format('Hi', 'Hello', 'Bonzor'))
+
+print("can insert by assigning keyword First: {a}, Second: {b}, Third: {c} .format(a=1,b='Two',c=12.3)")
+
+print('can insert by assigning keyword First: {a}, Second: {b}, Third: {c}'.format(a=1, b='abc', c=1.25))
+
+print('{0:8} | {1:9}'.format('Fruit', 'Quantity'))
+print('{0:8} | {1:9}'.format('Apples', 3.))
+print('{0:8} | {1:9}'.format('Oranges', 10))
+
+print('{0:=<8} | {1:-^8} | {2:.>8}'.format('Left','Center','Right'))
+print('{0:=<8} | {1:-^8} | {2:.>8}'.format(11,22,33))
+
+print('{0:=<8} | {1:-^8} | {2:.>8}'.format('Left','Center','Right'))
+print('{0:=<8} | {1:-^8} | {2:.>8}'.format(11,22,33))
 
