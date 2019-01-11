de62b319e5fd87425a2f3a1f495bc76ac784cc64
SandeepThakare <sandythakare9@gmail.com>
2019-01-11 10:37:38 +0530

    initia commit with variable and numbers

---
+++ numbers_variables.py
@@ -0,0 +1,12 @@
+a=10 #assign to a
+
+print("a is = ", a) #print a
+
+print("Value is = ", a/3) #returns float value
+
+print("Value is = ", a//3) #returns integer value without return
+
+print("Modulo of a%6 = ", a%6) # modulo operator
+
+print("Power of - a**3  = ", a**3) #calculate power of 
+
